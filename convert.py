aeb713001e9f7cf887f5ee40fad47766bf495c9d
Tibrella <pinghigh24678@outlook.com>
2023-05-16 19:41:13 +0800

    2023-05-16 11:41:11

--- convert.py
+++ convert.py
@@ -7,3 +7,15 @@ cvter = ObsidianToHugo(
 )
 
 cvter.run()
+
+os.popen("hugo -s T:/Source/blog")
+
+from datetime import datetime, timedelta
+now_time = datetime.now()
+utc_time = now_time - timedelta(hours=8)  # 减去8小时
+utc_time = utc_time.strftime("%Y-%m-%d %H:%M:%S")
+
+os.popen("cd T:/Source/blog && hugo")
+os.popen("git -C T:/Source/blog/public add .")
+os.popen("git -C T:/Source/blog/public commit -m \"{}\"".format(utc_time))
+os.popen("git -C T:/Source/blog/public push")
