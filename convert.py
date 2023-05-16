dd44eaabe885cee55baa05fb699c6d8b17dd4123
Tibrella <pinghigh24678@outlook.com>
2023-05-16 19:46:58 +0800

    2023-05-16 11:46:55

--- convert.py
+++ convert.py
@@ -15,7 +15,7 @@ now_time = datetime.now()
 utc_time = now_time - timedelta(hours=8)  # 减去8小时
 utc_time = utc_time.strftime("%Y-%m-%d %H:%M:%S")
 
-os.popen("cd T:/Source/blog && hugo")
-os.popen("git -C T:/Source/blog/public add .")
-os.popen("git -C T:/Source/blog/public commit -m \"{}\"".format(utc_time))
-os.popen("git -C T:/Source/blog/public push")
+os.system("hugo -s T:/Source/blog")
+os.system("git -C T:/Source/blog/public add .")
+os.system("git -C T:/Source/blog/public commit -m \"{}\"".format(utc_time))
+os.system("git -C T:/Source/blog/public push")
