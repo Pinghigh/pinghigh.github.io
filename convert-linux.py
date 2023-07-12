043d08fc323e587dd74af7d484250a431211d4af
Tibrella <pinghigh24678@outlook.com>
2023-07-12 15:01:06 +0800

    2023-07-12 15:01:04

--- convert-linux.py
+++ convert-linux.py
@@ -1,8 +1,5 @@
-#!/bin/python
 from obsidian_to_hugo import ObsidianToHugo
 import os
-
-hugo_dir = "../../../Source/blog"
 
 cvter = ObsidianToHugo(
     obsidian_vault_dir = os.path.abspath(os.path.join(os.getcwd(), "./")),
@@ -11,12 +8,14 @@ cvter = ObsidianToHugo(
 
 cvter.run()
 
+os.popen("hugo -s T:/Source/blog")
+
 from datetime import datetime, timedelta
 now_time = datetime.now()
 utc_time = now_time # 减去8小时
 utc_time = utc_time.strftime("%Y-%m-%d %H:%M:%S")
 
-os.system(f"hugo -s {hugo_dir}")
-os.system(f"git -C {hugo_dir}/public add .")
-os.system(f"git -C {hugo_dir}/public commit -m \"{utc_time}\"")
-os.system(f"git -C {hugo_dir}/public push")
+os.system("hugo -s T:/Source/blog")
+os.system("git -C T:/Source/blog/public add .")
+os.system("git -C T:/Source/blog/public commit -m \"{}\"".format(utc_time))
+os.system("git -C T:/Source/blog/public push")
